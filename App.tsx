f5e8482ad2e560e42e39627a814ec77e2b0db61b
Nemanja Grujic <luxisoop@gmail.com>
2022-03-25 13:34:44 -0600

    getting started

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TextInputComponent, View } from 'react-native';
 
 export default function App() {
+  const handleLogIn = () => {
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style = {styles.heading}>Uloguj se</Text>
+      <TextInput style={styles.input}>Korisničko Ime</TextInput>
+      <TextInput style={styles.input}>Lozinka</TextInput>
+      <Button title='Uloguj Se' onPress={handleLogIn}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 35,
+  },
+  input: {
+    fontSize: 20,
+    marginTop: 20,
+    borderRadius: 10,
+    borderColor: 'gray',
+    borderWidth: 2,
+    opacity: 2,
+    paddingHorizontal: 10,
+    width: 200,
+    paddingVertical: 5,
+  },
+  button: {
+    fontSize: 20,
+    backgroundColor: 'red',
+  }
 });
